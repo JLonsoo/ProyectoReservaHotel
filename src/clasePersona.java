1a5dee7d28138b2cf4c8500ad25549d61bee1066
Lonso <jalonsoocampo03gmail.com>
2025-11-29 23:47:20 -0500

    Initial commit del proyecto y clases base

---
+++ src/clasePersona.java
@@ -0,0 +1,10 @@
+
+public class clasePersona {
+    private String nombre;
+    private String apellido;
+    private String DNI;
+    private String telefono;
+    private String correoElectronico;
+    
+    
+}
